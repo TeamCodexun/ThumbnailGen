3cff2a2d522e77a723ffa8825678c5a691f766a8
Chuutt <93823376+Chuutt@users.noreply.github.com>
2022-08-23 18:45:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "BroadcastBot")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5618782891:AAHaNxV7WgvgqrPN6FdgVo1aFBSfkhj-xDs")
+API_ID = int(os.environ.get("API_ID", "10098309"))
+API_HASH = os.environ.get("API_HASH", "aaacac243dddc9f0433c89cab8efe323")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001587861988"))
+AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "2056407064").split())
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://codexun:TeamCodexun07@codexun.egmx5.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DB_NAME", "ThumbnailBot")
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
