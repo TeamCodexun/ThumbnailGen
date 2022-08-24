740bba495c3bc8fc7c7bfffe9e5820998bfa5005
Chuutt <93823376+Chuutt@users.noreply.github.com>
2022-08-24 22:17:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -96,10 +96,24 @@ START_BTN = InlineKeyboardMarkup(
         ]]
     )
 
+@Bot.on_message(filters.private & filters.command(["start", "help"]))
+async def start_help_handler(
+    bot : Update,
+    msg : Message
+    ):
+    if await search_user_in_community(bot, msg):
+        if msg.text == "/start":
+            textMsg = f"<b>Hi, I am MegaUploaderBot🤖 Created by @AJPyroVerse and My Developer🧑‍💻 is @AJTimePyro.</b>\n\nAnd I support:-\n1. <u>Direct Downloading Link</u>\n2.<u>Telegram File</u>\n3. <u>Youtube URL</u>\n\n\n to login\n😊We will store your login detail on our database."
+        else:
+            textMsg = f"to login\n<b>After login😊 send Direct Downloading Link, Youtube URL or any Telegram File.\n\nTo remove your account from Database use /revoke.</b>common text"
+        await msg.reply_text(
+            textMsg,
+            parse_mode = "html"
+        )
+    return
 
-@Bot.on_message(filters.command(["start"]))
+@Bot.on_message(filters.command(["sart"]))
 async def start(client: Client, message: Message):
-if await search_user_in_community(bot, msg):
       # return
     chat_id = message.from_user.id
     if not await db.is_user_exist(chat_id):
