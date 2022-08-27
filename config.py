2499af970f3a65777041645b09c1e4ea8e522381
Team Codexun <112056097+TeamCodexun@users.noreply.github.com>
2022-08-27 10:56:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5618782891:AAHaNxV7WgvgqrPN6FdgVo1aFBSfkhj-xDs")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5732918427:AAFGPW6p5OUPRqdqBbGocKOa-H-AstXdH_0")
 API_ID = int(os.environ.get("API_ID", "10098309"))
 API_HASH = os.environ.get("API_HASH", "aaacac243dddc9f0433c89cab8efe323")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001587861988"))
